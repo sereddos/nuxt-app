eacf44bb2a044bb180309f4c7d742be82a6155a9
Aleksey Sereda <sereddos@gmail.com>
2020-04-09 11:02:28 +0300

    init

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab labore obcaecati accusamus exercitationem nobis, consequuntur maiores? Distinctio autem deleniti laborum incidunt neque nisi fugit. Pariatur voluptatum reiciendis nobis excepturi ipsa!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita magni ut eligendi? Atque possimus nesciunt dignissimos officiis voluptates expedita quia, facilis quisquam eius ullam illum voluptate quis vel debitis! Impedit!</p>
+  </section>
+</template>
---
+++ pages/users/_id.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h1>User {{this.$route.params.id}}</h1>
+  </section>
+</template>
