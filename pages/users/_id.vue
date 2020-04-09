cfce33a059945e6e413cba345664f74351495de8
Aleksey Sereda <sereddos@gmail.com>
2020-04-09 12:36:07 +0300

    add more user info

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <h1>User {{this.$route.params.id}}</h1>
+    <h1>User page</h1>
+    <hr>
+    <p>Name: {{user.name}}</p>
+    <p>Email: {{user.email}}</p>
+    <p>Company: {{user.company.name}}</p>
   </section>
 </template>
 
@@ -8,6 +12,10 @@
 export default {
   validate({params}) {
     return /^\d+$/.test(params.id)
+  },
+  async asyncData({$axios, params}) {
+    const user = await $axios.$get('https://jsonplaceholder.typicode.com/users/' + params.id)
+    return {user}
   }
 }
 </script>
